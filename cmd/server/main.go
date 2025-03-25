5b555488f50a64221acd322c8b3fd834a5434fe2
Sokolnikov Viktor <vk.sokolnikov@gmail.com>
2025-03-25 08:23:05 +0300

    fix: fix test + format

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Metric interface {
-	Set(v string, t string) (error)
-	GetValueString() (string)
-	GetType() (string)
+	Set(v string, t string) error
+	GetValueString() string
+	GetType() string
 }
 
 type GaugeMetric struct {
-	Name    string
-	Type	string
-	Value   float64
+	Name  string
+	Type  string
+	Value float64
 }
 
-func (g *GaugeMetric) Set(v string, t string) (error) {
+func (g *GaugeMetric) Set(v string, t string) error {
 	nv, err := strconv.ParseFloat(v, 64)
 	if err != nil {
 		return err
@@ -29,21 +29,21 @@ func (g *GaugeMetric) Set(v string, t string) (error) {
 	return nil
 }
 
-func (g *GaugeMetric) GetValueString() (string) {
+func (g *GaugeMetric) GetValueString() string {
 	return fmt.Sprintf("%f", g.Value)
 }
 
-func (g *GaugeMetric) GetType() (string) {
+func (g *GaugeMetric) GetType() string {
 	return g.Type
 }
 
 type CounterMetric struct {
-	Name    string
-	Type	string
-	Value   int64
+	Name  string
+	Type  string
+	Value int64
 }
 
-func (c *CounterMetric) Set(v string, t string) (error) {
+func (c *CounterMetric) Set(v string, t string) error {
 	nv, err := strconv.ParseInt(v, 10, 64)
 	if err != nil {
 		return err
@@ -53,15 +53,15 @@ func (c *CounterMetric) Set(v string, t string) (error) {
 	return nil
 }
 
-func (g *CounterMetric) GetValueString() (string) {
+func (g *CounterMetric) GetValueString() string {
 	return fmt.Sprintf("%d", g.Value)
 }
 
-func (c *CounterMetric) GetType() (string) {
+func (c *CounterMetric) GetType() string {
 	return c.Type
 }
 
-func SetMetricValue(m Metric, v string, t string) (error) {
+func SetMetricValue(m Metric, v string, t string) error {
 	err := m.Set(v, t)
 	return err
 }
@@ -70,12 +70,12 @@ type MetricStorage struct {
 	metrics map[string]Metric
 }
 
-func (ms MetricStorage) CreateMetric(n string, m Metric) (Metric) {
+func (ms MetricStorage) CreateMetric(n string, m Metric) Metric {
 	ms.metrics[n] = m
 	return m
 }
 
-func (ms MetricStorage) GetMetrics(n string) (Metric) {
+func (ms MetricStorage) GetMetrics(n string) Metric {
 	m, ok := ms.metrics[n]
 	if !ok {
 		return nil
@@ -109,7 +109,7 @@ func updateMetricHandler(res http.ResponseWriter, req *http.Request) {
 		}
 		err := SetMetricValue(metric, metricValue, metricType)
 		if err != nil {
-			res.WriteHeader(http.StatusInternalServerError)
+			res.WriteHeader(http.StatusBadRequest)
 		}
 
 		metricStorage.CreateMetric(metricName, metric)
