bcc2723fd5dee91c2db76000e1cab89f480546d7
Sokolnikov Viktor <vk.sokolnikov@gmail.com>
2025-04-06 00:16:47 +0300

    fix agent config

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"time"
 
-	"github.com/caarlos0/env/v6"
 	"github.com/sincin-v/collector/internal/agent/clients/rest"
 	"github.com/sincin-v/collector/internal/agent/config"
 	"github.com/sincin-v/collector/internal/agent/helpers/metrics"
@@ -15,50 +13,18 @@ import (
 
 func main() {
 	log.Printf("Start agent work")
+	agentConfig := config.GetAgentConfig()
 
-	var argServerHost = flag.String("a", "localhost:8080", "Metric server host and port")
-	var argReportInterval = flag.Int("r", 10, "Report interval")
-	var argPollInterval = flag.Int("p", 2, "Poll interval")
-
-	flag.Parse()
-
-	var cfg config.Config
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Printf("Env is empty")
-	}
-
-	var serverHost string
-	if cfg.Address != "" {
-		serverHost = cfg.Address
-	} else {
-		serverHost = *argServerHost
-	}
-	var reportInterval time.Duration
-	if cfg.ReportInterval != 0 {
-		reportInterval = cfg.ReportInterval
-	} else {
-		reportInterval = time.Duration(time.Duration(*argReportInterval) * time.Second)
-	}
-
-	var pollInterval time.Duration
-	if cfg.PollInterval != 0 {
-		pollInterval = cfg.PollInterval
-	} else {
-		pollInterval = time.Duration(time.Duration(*argPollInterval) * time.Second)
-
-	}
-
-	log.Printf("Send metrics to %s", serverHost)
+	log.Printf("Send metrics to %s", agentConfig.ServerHost)
 	metricStorage := storage.New()
 	service := service.New(&metricStorage)
-	hc := rest.New(serverHost)
+	hc := rest.New(agentConfig.ServerHost)
 	metricsCollector := metrics.New(&service, hc)
-	go metricsCollector.StartSendMetrics(reportInterval)
+	go metricsCollector.StartSendMetrics(agentConfig.ReportInterval)
 	for {
 		// go metricsCollector.StartCollectMetrics(pollInterval)
 		go metricsCollector.CollectMetrics()
 
-		time.Sleep(pollInterval)
+		time.Sleep(agentConfig.PollInterval)
 	}
 }
--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -1,11 +1,57 @@
 package config
 
 import (
+	"flag"
+	"log"
 	"time"
+
+	"github.com/caarlos0/env/v6"
 )
 
-type Config struct {
+type EnvConfig struct {
 	Address        string        `env:"ADDRESS"`
 	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
 	PollInterval   time.Duration `env:"POLL_INTERVAL"`
+}
+
+type AgentConfig struct {
+	ServerHost     string
+	ReportInterval time.Duration
+	PollInterval   time.Duration
+}
+
+func GetAgentConfig() AgentConfig {
+	var cfg AgentConfig
+	var envCfg EnvConfig
+
+	var argServerHost = flag.String("a", "localhost:8080", "Metric server host and port")
+	var argReportInterval = flag.Int("r", 10, "Report interval")
+	var argPollInterval = flag.Int("p", 2, "Poll interval")
+
+	flag.Parse()
+
+	err := env.Parse(&envCfg)
+	if err != nil {
+		log.Printf("Env is empty")
+	}
+
+	if envCfg.Address != "" {
+		cfg.ServerHost = envCfg.Address
+	} else {
+		cfg.ServerHost = *argServerHost
+	}
+	if envCfg.ReportInterval != 0 {
+		cfg.ReportInterval = envCfg.ReportInterval
+	} else {
+		cfg.ReportInterval = time.Duration(time.Duration(*argReportInterval) * time.Second)
+	}
+
+	if envCfg.PollInterval != 0 {
+		cfg.PollInterval = envCfg.PollInterval
+	} else {
+		cfg.PollInterval = time.Duration(time.Duration(*argPollInterval) * time.Second)
+
+	}
+
+	return cfg
 }
